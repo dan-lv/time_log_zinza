ff740eba22d6798789b05295c99d615228308970
daniel <nhudanmkt@gmail.com>
2019-12-17 16:35:24 +0700

    fix: ui

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,11 +1,10 @@
-@extends('layouts.app')
+@extends('layout')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+        <div class="col-md-8 mt-5">
+            <div class="card form-absent">
 
                 <div class="card-body">
                     @if (session('status'))
@@ -33,7 +32,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-secondary">
                                     {{ __('Send Password Reset Link') }}
                                 </button>
                             </div>
